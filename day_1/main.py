ccc286382e6f3c8ef57f3a5f71590d542b6eec61
JustinRoderick <justinroderick258@gmail.com>
2024-12-04 23:09:04 -0500

    day_1 part 1 done

--- day_1/main.py
+++ day_1/main.py
@@ -1,2 +1,17 @@
+# Day 1 solutions for part 1 and 2
 def part1():
-    return
+    file = open("input.txt", "r")
+    left = []
+    right = []
+    total = 0
+    for line in file:
+        splitted = line.split()
+        left.append(int(splitted[0]))
+        right.append(int(splitted[1]))
+    left.sort()
+    right.sort()
+    for i in range(len(left)):
+        total += abs(left[i] - right[i])        
+    return total
+
+print(part1())

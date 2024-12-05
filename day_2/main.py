9350fd3c719ce05bfd528a32331d2f9610fff234
JustinRoderick <justinroderick258@gmail.com>
2024-12-05 00:17:27 -0500

    day 2 part 1

---
+++ day_2/main.py
@@ -0,0 +1,17 @@
+def is_safe(report):
+    is_increasing = all(report[i+1] - report[i] >= 1 and report[i+1] - report[i] <= 3 for i in range(len(report) - 1))
+    is_decreasing = all(report[i] - report[i+1] >= 1 and report[i] - report[i+1] <= 3 for i in range(len(report) - 1))
+    return is_increasing or is_decreasing
+
+def part1():
+    file = open("input.txt", "r")
+    count = 0
+    for line in file:
+        report = list(map(int, line.strip().split()))
+        if is_safe(report):
+            count += 1
+            continue
+    
+    return count 
+
+print(part1())

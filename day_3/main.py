16ebe208bc0df9618c946c9dfd970ef43153fc77
JustinRoderick <justinroderick258@gmail.com>
2024-12-05 23:31:42 -0500

    day 3 part 2 done

--- day_3/main.py
+++ day_3/main.py
@@ -16,6 +16,18 @@ print(part1())
 
 def part2():
     file = open("input.txt", "r")
-    
-    
+    total = 0
+    string = file.read()
+    found = re.findall("mul\(\d+,\d+\)|do\(\)|don't\(\)", string)
+    flag = True
+    for i in found:
+        if i == "do()":
+            flag = True
+        elif i == "don't()":
+            flag = False
+        elif flag:
+            a, b = map(int, re.findall("\d+", i))
+            total += a * b
+    return total
+
 print(part2())

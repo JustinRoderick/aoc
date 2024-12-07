f8b4109c5d5db29603a38d3759e1c80f6149e25e
JustinRoderick <justinroderick258@gmail.com>
2024-12-06 23:36:21 -0500

    day 4 part 1 done I think, will finish later

---
+++ day_4/main.py
@@ -0,0 +1,32 @@
+
+def part1():
+    file = open("input.txt", "r")
+    word_search = file.read()
+    row, col = len(word_search), len(word_search[0])
+    total_count = 0
+    target = 'XMAS'
+    directions = [
+        (0,1),
+        (0,-1),
+        (1,0),
+        (-1,0),
+        (1,1),
+        (-1,-1),
+        (1,-1),
+        (-1,1)
+    ]
+    
+    def is_valid(x, y, dx, dy):
+        for i in range(len(target)):
+            nx, ny = x + i * dx, y + i * dy
+            if not (0 <= nx < row and 0 <= ny < col) or word_search[nx][ny] != target[i]:
+                return False
+        return True
+
+    
+    for r in range(row):
+        for c in range(col):
+            for dx, dy in directions:
+                if is_valid(r, c, dx, dy):
+                    total_count += 1
+    return total_count

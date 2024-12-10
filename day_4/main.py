ab9487b9b3e41cbf14ba571f9aec00feb011d31a
JustinRoderick <justinroderick258@gmail.com>
2024-12-09 23:41:23 -0500

    bad solution but whatever

--- day_4/main.py
+++ day_4/main.py
@@ -33,3 +33,36 @@ def part1():
     return total_count
 
 print(part1())
+
+def part2():
+    file = open("input.txt", "r")
+    word_search = file.read().split("\n")
+    row, col = len(word_search), len(word_search[0])
+    total_count = 0
+    target = "MMSS"
+    directions = [
+        (1,1),
+        (-1,-1),
+    ]
+    
+    def is_valid(x, y, dx):
+        
+        if not (0 <= x+1 < row and 0 <= x-1 < row and 0 <= y+1 < col and 0 <= y-1 < col):
+            return False
+        if(dx == -1):
+            if(word_search[x+dx][y+1] != 'M' or word_search[x+dx][y-1] != 'M'):
+                return False
+        if(dx == 1):
+            if(word_search[x+dx][y+1] != 'S' or word_search[x+dx][y-1] != 'S'):
+                return False
+        return True
+    
+    for r in range(row):
+        for c in range(col):
+            if(word_search[r][c] == 'A'):
+                if is_valid(r, c, -1) and is_valid(r, c, 1):
+                    total_count += 1
+                    
+    return total_count
+
+print(part2())
